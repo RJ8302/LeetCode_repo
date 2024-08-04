7e20f2886c553d70538b149dd59e77ce80954515
RJ8302 <143059108+RJ8302@users.noreply.github.com>
2024-08-04 22:57:12 +0530

    Time: 2 ms (22.71%), Space: 44.6 MB (22.61%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+         if (n == 0) return new ArrayList<>();
+        return generate(1, n);
+    }
+    
+    private List<TreeNode> generate(int start, int end) {
+        List<TreeNode> allTrees = new ArrayList<>();
+        if (start > end) {
+            allTrees.add(null);
+            return allTrees;
+        }
+
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftTrees = generate(start, i - 1);
+            List<TreeNode> rightTrees = generate(i + 1, end);
+
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right : rightTrees) {
+                    TreeNode currentTree = new TreeNode(i);
+                    currentTree.left = left;
+                    currentTree.right = right;
+                    allTrees.add(currentTree);
+                }
+            }
+        }
+
+        return allTrees;
+    }
+}

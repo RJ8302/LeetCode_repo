e359f55ce04468a957c3be30bea6e5f058851bed
RJ8302 <143059108+RJ8302@users.noreply.github.com>
2024-08-05 23:10:58 +0530

    Time: 22 ms (56.98%), Space: 44.6 MB (73.68%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+         int m = s.length();
+        int n = p.length();
+        
+        // dp[i][j] will be true if s[0..i-1] matches p[0..j-1]
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        
+        // Base case: empty string matches empty pattern
+        dp[0][0] = true;
+        
+        // Base case: empty string and non-empty pattern
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+        
+        // Fill the table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    // '*' can match zero or more characters
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                } else if (p.charAt(j - 1) == '?' || s.charAt(i - 1) == p.charAt(j - 1)) {
+                    // '?' matches any single character or exact character match
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+            }
+        }
+        
+        return dp[m][n];   
+    }
+}
